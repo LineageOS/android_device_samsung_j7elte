d4994ca2f5bb1043ff0b2421f3f5c75a4d955634
Danny Wood <danwood76@gmail.com>
2018-10-01 19:49:22 +0100

    j7elte: Add wifi config files
    Change-Id: I7d54c64dbbb9b726647118f3ce8a86c18737d29e

--- device.mk
+++ device.mk
@@ -65,6 +65,13 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     init.target.rc
 
+# Wi-fi
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/wifi/cred.conf:system/etc/wifi/cred.conf \
+    $(LOCAL_PATH)/configs/wifi/wpa_supplicant_overlay.conf:system/etc/wifi/wpa_supplicant_overlay.conf \
+    $(LOCAL_PATH)/configs/wifi/p2p_supplicant_overlay.conf:system/etc/wifi/p2p_supplicant_overlay.conf \
+    $(LOCAL_PATH)/configs/wifi/filter_ie:system/etc/wifi/filter_ie
+
 # Inherit from universal7580-common
 $(call inherit-product, device/samsung/universal7580-common/device-common.mk)
 
